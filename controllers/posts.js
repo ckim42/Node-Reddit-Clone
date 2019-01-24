7c14796b2ebd815e5dfdd8d7ef00de183b17d626
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-23 20:20:45 -0800

    users can view individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,7 +8,9 @@ module.exports = app => {
   app.get('/', (req, res) => {
     Post.find({})
       .then(posts => {
-        res.render("posts-index", { posts });
+        res.render("posts-index", {
+          posts
+        });
       })
       .catch(err => {
         console.log(err.message);
@@ -27,6 +29,20 @@ module.exports = app => {
     post.save((err, post) => { //save instance of post model to db
       return res.redirect('/'); //redirects to the root
     })
+  });
+
+  // SHOW
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id)
+      .then(post => {
+        res.render("posts-show", {
+          post
+        });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   });
 
 };
