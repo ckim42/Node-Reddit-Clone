b79c18ccf15411ab2c671a3f3b129d6c6061422c
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-24 23:03:06 -0800

    usernames of comments` authors should be visible but aren`t (but WHY??)

--- controllers/posts.js
+++ controllers/posts.js
@@ -46,11 +46,12 @@ module.exports = function(app) {
   app.get('/posts/:id', function(req, res) {
     var currentUser = req.user;
     // LOOK UP THE POST
-    Post.findById(req.params.id).populate('comments').populate('author').then((post) => {
-      res.render('posts-show', { post })
-    }).catch(err => {
+    Post.findById(req.params.id).populate({path: 'comments', populate: {path: 'author'}}).populate('author')
+      .then(post => {
+        res.render('posts-show', { post, currentUser });
+      }).catch(err => {
         console.log(err.message);
-    });
+      });
   });
 
   // Subreddit
