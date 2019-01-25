482d015bc4795299681762a016e064bbfc1eb483
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-24 20:45:28 -0800

    Revert "users can log in"
    This reverts commit 05c82a9f7458a6eb73c8109853c457a422c3e250.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,4 @@
-const Post = require('../models/post');
-const Comment = require('../models/comment');
-const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
 
@@ -31,38 +27,6 @@ module.exports = (app) => {
   app.get('/logout', (req, res) => {
     res.clearCookie('nToken');
     res.redirect('/');
-  });
-
-  // LOGIN FORM
-  app.get('/login', (req, res) => {
-    res.render('login');
-  });
-
-  // ACTUALLY LOGGING IN
-  app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const user = new User(req.body);
-
-    // Find this user name
-    User.findOne({ username }, "username password")
-      .then(user => {
-        if (!user) { // User not found
-          return res.status(401).send({ message: "Wrong Username or Password" });
-        }
-        user.comparePassword(password, (err, isMatch) => { // Checks password
-          if (!isMatch) { // Password doesn't match
-            return res.status(401).send({ message: "Wrong Username or password" });
-          }
-          const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { // Creates a token
-            expiresIn: "60 days"
-          });
-          res.cookie("nToken", token, { maxAge: 900000, httpOnly: true }); // Sets a cookie and redirects to root view
-          res.redirect("/");
-        });
-      }).catch(err => {
-        console.log(err);
-      });
   });
 
 }
