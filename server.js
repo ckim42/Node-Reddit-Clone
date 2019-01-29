3a449965df0004d2273cd3fffe2664e0adce97fb
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-29 00:18:07 -0800

    attempted implementing nested comments (not yet fxnl) & updated todo.txt

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ const Post = require('./models/post');
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+require('./controllers/replies.js')(app);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
